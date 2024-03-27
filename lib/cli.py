eb60e5cd629fcd580abbc33ea39c26a051787cef
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-03-26 18:18:37 -0700

    cli updates

--- lib/cli.py
+++ lib/cli.py
@@ -8,42 +8,72 @@ from helpers import (
 
 def main():
     while True:
-        menu()
+        main_menu()
         choice = input("> ")
         if choice == "0":
             exit_program()
         elif choice == "1":
             user = find_or_create_username()
+            # START OF SUB MENU 1 - CREATED USERNAME
             while True:
-                menu()
+                sub_menu_1()
                 choice = input("> ")
                 if choice == "0":
                     exit_program()
                 elif choice == "1":
-                    user = find_or_create_username()
+                    # Browse activities in Portland. Kia
+                    pass
+                elif choice == "2":
+                    # View saved activities. Xen
+                    pass
+                elif choice == "3":
+                    # Delete user. Steph
+                    pass
                 else:
                     print("Invalid choice")
+            # END OF SUB MENU 1
         else:
             print("Invalid choice")
 
-# def main_menu():
-#     print("Please select an option:")
-#     print("0. Exit the program")
-#     print("1. Browse activities in Portland.")
-            
 
 
-
-def menu():
-    print("Please select an option:")
+# Starting program
+def main_menu():
+    print("To get started, please create a username:")
     print("0. Exit the program")
     print("1. Create a username.")
-    print("2. Browse activities in Portland.")
-    print("3. View saved activities.")
-    print("4. Delete user.")
+
+# After create username
+def sub_menu_1():
+    print("Please select an option:")
+    print("0. Exit the program")
+    print("1. Browse activities in Portland.")
+    print("2. View saved activities.")
+    print("3. Delete user.")
+
+# Clicked "Browse Activities"
+def sub_menu_2():
+    print("Please select an option:")
+    print("0. Exit the program")
+    print("1. Browse all activities.")
+    print("2. Find activities by type.")
+    print("3. Find activities by name.")
+    print("4. Find activities by neighborhood.")
+    print("5. Find by rating.")
+    print("6. View saved activities.")
+
+# Clicked "View Saved Activities"
+def sub_menu_3():
+    print("Please select an option:")
+    print("0. Exit the program")
+    print("1. View all saved activities.")
+    print("2. Add rating & review to activity.")
+    print("3. Delete activity from saved list.")
+    print("4. Browse all activities.")
+            
 
 if __name__ == "__main__":
     welcome()
-    main()
+    main_menu()
 
 
